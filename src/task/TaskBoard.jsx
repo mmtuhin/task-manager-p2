727a964fcc90c4e3568d002f1487268e8ed56894
Tuhin Hossain <mmtuhinhossain@gmail.com>
2024-01-23 09:04:51 +0600

    Add task functionality added

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -14,15 +15,44 @@ const TaskBoard = () => {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showModal, setShowModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
+
+  //console.log(showModal);
+  const handleAddTask = (newTask, isAdd) => {
+    console.log("Adding a Task....", newTask);
+    // console.log(showModal);
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
+
+    setShowModal(false);
+  };
+  const handlEditTask = (task) => {
+    setTaskToUpdate(task);
+    setShowModal(true);
+  };
   return (
     <section className="mb-20" id="tasks">
+      {showModal && (
+        <AddTaskModal onSave={handleAddTask} taskToUpdate={taskToUpdate} />
+      )}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
-          <TaskList tasks={tasks} />
+          <TaskActions onAddClick={() => setShowModal(true)} />
+          <TaskList tasks={tasks} onEdit={handlEditTask} />
         </div>
       </div>
     </section>
